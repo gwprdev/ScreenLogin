409277403cebc9a27d0304a557e19ed9f1b1b816
gwprdev <gwprdev@gmail.com>
2022-12-14 01:34:16 -0300

    teste

---
+++ arquiteturas/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  arquiteturas
+//
+//  Created by Gilvan Wemerson on 13/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
